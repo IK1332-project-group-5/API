d7f7ff5ec65936a9465b67c9eda9bc477ebb1158
Basoz Yar <basoz@kth.se>
2026-02-09 12:05:18 +0100

    render port support

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ app.get("/data", (req,res) => {
   res.json(store);
 });
 
-app.listen(3000, () =>
-  console.log("API running on port 3000")
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log("API running on port", PORT)
 );
 
